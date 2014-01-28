3c0292873cc7bc4ca71bd8d934f45d0dbe72413c
Tony Vu <anthonyqvu@gmail.com>
2014-01-28 10:18:31 -0500

    first commit

---
+++ app/controllers/dashboards_controller.rb
@@ -0,0 +1,8 @@
+class DashboardsController < ApplicationController
+  
+  def show
+    @shout = Shout.new
+    @shouts = current_user.shouts
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shouter::Application.config.secret_token = '142aba1097327d8f5628ed4c3f77c0acde0d55a75179e8955c0aeb22b0e34ded25e84c24fa60b8b5bc715c6e79b5bb2ca423176b40c4b6a2bd75b8e19884d26c'
